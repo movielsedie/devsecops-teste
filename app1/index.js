a06713a5b7b466bd5dbb6aa3cf48f0f0c73bbd90
Jose Albano <filippe.albano@gmail.com>
2021-09-24 06:58:06 -0700

    adicionando security headers

--- app1/index.js
+++ app1/index.js
@@ -6,6 +6,20 @@ function handleRequest(req, res) {
   res.end()
 }
 
+function handler(event) {
+  var response = event.response;
+  var headers = response.headers;
+
+
+  headers['strict-transport-security'] = { value: 'max-age=63072000; includeSubdomains; preload'}; 
+  headers['content-security-policy'] = { value: "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"}; 
+  headers['x-content-type-options'] = { value: 'nosniff'}; 
+  headers['x-frame-options'] = {value: 'DENY'}; 
+  headers['x-xss-protection'] = {value: '1; mode=block'}; 
+
+  return response;
+}
+
 const PROFILES_LIST = [
   {
     name: 'default',
